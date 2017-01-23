258248a6bf9617b7edc3f8ab5450189f75206c8d
Dario Tecchia <Dario Tecchia>
2017-01-23 22:43:13 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+(function(){
+
+  'use strict';
+
+  angular
+    .module('app', [])
+    .controller('AppController', AppController);
+
+  AppController.$inject = ['$scope', '$http'];
+
+  function AppController($scope, $http) {
+
+    var ac = this;
+
+    ac.initialize = initialize;
+
+    ac.initialize();
+
+    return ac;
+
+    function initialize() {
+
+      console.log('AppController');
+
+      $http.get("assets/skills.json").then(function(skills) {
+        $scope.skills = skills.data;
+      });
+      $http.get("assets/experiences.json").then(function(experiences) {
+        $scope.experiences = experiences.data;
+      });
+      $http.get("assets/projects.json").then(function(projects) {
+        $scope.projects = projects.data;
+      });
+
+    }
+
+  }
+
+})();
